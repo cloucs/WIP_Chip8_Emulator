6a570bb9646f493804b157ddd09f3eec27dd7677
Melvin Ziemann <ziemann.melvin@gmail.com>
2021-08-18 07:46:20 +0200

    initialized the first members and functions

---
+++ Chip8/main.cpp
@@ -0,0 +1,87 @@
+//imports
+#include <cstdint>
+#include <fstream>
+
+//global variables
+
+class Chip8
+{
+public:
+	//constructor
+	Chip8();
+
+	//member variables
+	uint8_t registers[16]{};
+	uint8_t memory[4096]{};
+	uint16_t index{};
+	uint16_t pc{};
+	uint16_t stack[16]{};
+	uint8_t delayTimer{};
+	uint8_t soundTimer{};
+	uint8_t keypad[16]{};
+	uint32_t video[64 * 32]{};
+	uint16_t opcode;
+
+	const unsigned int START_ADDRESS = 0x200;
+	const unsigned int FONTSET_SIZE = 80;
+
+	uint8_t fontset[FONTSET_SIZE] =
+	{
+		0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+		0x20, 0x60, 0x20, 0x20, 0x70, // 1
+		0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+		0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+		0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+		0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+		0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+		0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+		0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+		0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+		0xF0, 0x90, 0xE0, 0x90, 0x90, // A
+		0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+		0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+		0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+		0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+		0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+	};
+
+	//member functions
+	void LoadROM(char const* filename);
+};
+
+Chip8::Chip8()
+{
+	pc = START_ADDRESS;
+}
+
+void Chip8::LoadROM(char const* filename)
+{
+	//open the file as a stream of binary and move the file pointer to the end
+	std::ifstream file(filename, std::ios::binary | std::ios::ate);
+	
+	if (file.is_open())
+	{
+		//get size of file and allocate a buffer to hold the contents
+		std::streampos size = file.tellg();
+		char* buffer = new char[size];
+
+		//go back to the beginning of the file and fill the buffer
+		file.seekg(0, std::ios::beg);
+		file.read(buffer, size);
+		file.close();
+
+		//load the ROM contents into the Chip8's memory, starting at 0x200
+		for (long i = 0; i < size; ++i)
+		{
+			memory[START_ADDRESS + i] = buffer[i];
+		}
+
+		//free the buffer
+		delete[] buffer;
+	}
+}
+
+int main(int arg, char* argv[])
+{
+
+}
